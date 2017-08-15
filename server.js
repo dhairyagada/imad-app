f68d7ffe19a783b0e66d66e90120eb9558deb0d7
Dhairya <dhairyagada1997@gmail.com>
2017-08-15 15:29:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,54 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articleOne={
+  title:  'Article One | Dhairya Gada',
+  heading: 'First Article',
+  date: 'August 15 2017',
+  content: ` <p>
+                Hello Friends this is my first article on this webapp.
+            </p>
+            `
+};
+
+function createTemplate(data){
+var title=data.title;
+var heading=data.heading;
+var date=data.date;
+var content=data.content;
+var htmlTemplate =`
+<html>
+<head>
+   <title>${title}</title>
+   <link href="/ui/style.css" rel="stylesheet" />
+   <style>
+       
+
+   </style>
+</head>
+<body>
+    <div class="container">
+        <div><a href="/">Home</a></div>
+        <hr/>
+        <h3>${heading}</h3>
+        <div>${date}</div>
+        <div>
+            ${content}
+        </div>
+    </div>
+</body>
+</html>
+`;    
+return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one',function(req,res){
-res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+res.sendFile(createTemplate(articleOne));
 })
     
 
