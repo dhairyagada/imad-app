a307801f17d1756d0f35322e484fb00db38dfbc5
Dhairya <dhairyagada1997@gmail.com>
2017-09-28 11:57:43 +0530

    Revert "[imad-console] Updates server.js"
    This reverts commit a9cbd786d177793c55be8cac3df5a26f075f8adc.

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ return htmlTemplate;
 }
 
 app.get('/', function (req, res) {
-  res.send(path.join(__dirname, 'ui', 'index.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one',function(req,res){
